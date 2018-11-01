f68a7d1c3502c0e3109a07f9ae7a3eafe1cffd2e
caismith-ch <csmith2@companieshouse.gov.uk>
2018-11-01 10:12:23 +0000

    Rename DocumentGeneratorConsumerApplicationConfiguration to EnvironmentConfiguration and add tests

--- src/main/java/uk/gov/companieshouse/document/generator/consumer/configuration/EnvironmentConfiguration.java
+++ src/main/java/uk/gov/companieshouse/document/generator/consumer/configuration/EnvironmentConfiguration.java
@@ -1,15 +1,21 @@
-package uk.gov.companieshouse.document.generator.consumer;
+package uk.gov.companieshouse.document.generator.consumer.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
 
 @Configuration
-public class DocumentGeneratorConsumerApplicationConfiguration {
+public class EnvironmentConfiguration {
 
     @Bean
     EnvironmentReader environmentReader() {
         return new EnvironmentReaderImpl();
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
