0a66f2e9209707323e084854b7997c49b550e134
dgurjar-ch <dgurjar@companieshouse.gov.uk>
2024-12-09 14:37:46 +0000

    Setting EnableIdempotence as false for kafka producer

--- src/main/java/uk/gov/companieshouse/document/generator/consumer/kafka/impl/KafkaProducerServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/document/generator/consumer/kafka/impl/KafkaProducerServiceImpl.java
@@ -19,6 +19,7 @@ public class KafkaProducerServiceImpl implements KafkaProducerService {
     private static final Logger LOG = LoggerFactory.getLogger(DocumentGeneratorConsumerApplication.APPLICATION_NAME_SPACE);
 
     private CHKafkaProducer producer;
+    private ProducerConfig config;
 
     public KafkaProducerServiceImpl() {
 
@@ -32,6 +33,7 @@ public class KafkaProducerServiceImpl implements KafkaProducerService {
 
         ProducerConfigHelper.assignBrokerAddresses(producerConfig);
         producer = new CHKafkaProducer(producerConfig);
+        config.setEnableIdempotence(false);
     }
 
     @Override
